aa85e9c3716fab2a87c8df152d9d07f9186384ef
idoatk <109948971+idoatk@users.noreply.github.com>
2022-10-17 21:33:43 +0300

    Add files via upload

---
+++ WelcomePage.java
@@ -0,0 +1,22 @@
+package Mapping;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WelcomePage {
+    public static Class<Object> java;
+    WebDriver driver;
+
+    //define logo element as xpath
+    @FindBy(xpath = "//*[@id=\"header_logo\"]/a/img")
+    public static WebElement Logo;
+//define signinbutton as xpath
+    @FindBy(xpath ="//*[@id='header']/div[2]/div/div/nav/div[1]/a)")
+    public static WebElement SignInButton;
+
+    public WelcomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
